aedce412da9cdfd6efc8b2f9369af6217fd2305a
BoburBokijonov <64985667+BoburBokijonov@users.noreply.github.com>
2021-01-18 06:18:32 +0100

    Finished Web-site

---
+++ test-js.js
@@ -0,0 +1,10 @@
+function Circle (radius){
+    this.radius = radius;
+};
+
+Circle.prototype.getArea = function (){
+    return Math.PI * Math.pow(this.radius,2);
+}
+
+let myCircle = new Circle(10);
+console.log(myCircle.getArea());
